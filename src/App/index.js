58ed23f6e678730c85e8a96a406d650c9dabf864
Aaron Denney <aarondenney13@gmail.com>
2019-04-25 11:03:32 -0400

    set up working routes/links to components

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 import "./style.css";
+import { Route, Link } from "react-router-dom"
+import About from "./About/About"
+import Work from "./Work/Work"
+import Contact from "./Contact/Contact"
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div className="App">
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/work">My Work</Link>
+          <Link to="/contact">Contact Me</Link>
+        </nav>
+        <main>
+          <Route 
+            path="/about"
+            component={About}
+          />
+          <Route 
+            path="/work"
+            component={Work}
+          />
+          <Route 
+            path="/contact"
+            component={Contact}
+          />
+        </main>
+      </div>
+    )
   }
 }
 
