a10544cd06e18c490bf1a666009bffe8f0c645b7
Aaron Denney <aarondenney13@gmail.com>
2019-04-25 12:56:32 -0400

    set up keyframes animation so link will popup from bottom

--- src/App/Contact/Contact.js
+++ src/App/Contact/Contact.js
@@ -1,9 +1,11 @@
-import React from "react"
+import React from 'react'
 
 const Contact = () => {
     return (
-        <div className = "contact">
-            <h1>HIT ME UP!</h1>
+        <div className = 'contact-stuff'>
+            <h2>Contact Me</h2>
+            <p><a href='mailto:aarondenney13@gmail.com'>aarondenney13@gmail.com</a></p>
+            <p><a href='https://www.linkedin.com/in/aaron-denney/'>Linked In</a></p>
         </div>
     )
 }
